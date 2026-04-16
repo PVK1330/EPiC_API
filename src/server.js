991edb6419ade659bb52eb19a5dce69d892c296d
Prisha1801 <prince.anantkamalstudios@gmail.com>
2026-04-16 10:55:08 +0530

    Resolved stash conflict in server.js

--- src/server.js
+++ src/server.js
@@ -17,4 +17,26 @@ db.sequelize.sync().then(async () => {
 }).catch((err) => {
   console.error('Failed to connect to database:', err);
   process.exit(1);
-});
+});
+
+
+
+// require("dotenv").config();
+// const app = require("./app");
+// const db = require("./models");
+// const seedRoles = require("./seeders/role.seeder");
+// const seedAdmin = require("./seeders/admin.seeder");
+
+// const PORT = process.env.PORT || 5000;
+
+// db.sequelize.sync({ alter: true }).then(async () => {
+//   console.log("Database connected");
+
+//   // RUN SEEDER HERE
+//   await seedRoles();
+//   await seedAdmin();
+
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// });
